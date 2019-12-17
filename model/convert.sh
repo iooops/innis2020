14880db7b0dbef29fb136746f78712865f53deee
Miooo <digdongaa@gmail.com>
2019-12-17 18:00:37 +0800

    = =

--- model/convert.sh
+++ model/convert.sh
@@ -1,5 +1,6 @@
 tensorflowjs_converter \
-    --input_format=tf_frozen_model \
+    --input_format=tf_saved_model \
     --output_node_names='jsmodel' \
-    /frozen_inference_graph.pb \
-    /mobilenet/web_model
+    --saved_model_tags=serve \
+    . \
+    ./mobilenet/web_model
